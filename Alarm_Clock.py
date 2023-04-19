858773c7deaf843c5e9bec2c4d307e98152f7fda
Lukas <lzerucha@gmail.com>
2023-04-19 21:21:57 +0100

    Refactored into functions. Improved readability. Changed closing button

--- Alarm_Clock.py
+++ Alarm_Clock.py
@@ -1,103 +1,104 @@
-# Alarm Clock
 import PySimpleGUI as psg
 import time
 import winsound
 
-clock_font = "Digital-7"
-text_font = "Verdana"
-theme = psg.theme('black')
-
 
 def create_window():
-    psg.theme("black")
+    # Create the main window with two tabs: "Clock" and "Alarm"
 
     clock_layout = [
         [psg.VPush()],
-        [psg.Text("", font="Any 90", key="-TIME-")],
+        [psg.Text("", font=("Digital-7 Mono", 90), text_color="red", key="-TIME-")],
         [psg.VPush()]
     ]
 
     alarm_layout = [
         [psg.Text("HOUR", size=(9, 1)), psg.Text("MINUTE", size=(10, 1))],
         [psg.Input(key="-HR-", size=(10, 1)), psg.Input(key="-MIN-", size=(10, 1))],
-        [psg.Text("", size=(40, 5), key="-LST-")],
+        [psg.Text("", size=(40, 5), key="-ALARMS-LIST-")],
         [psg.Button("Confirm", button_color=("white", "black"), border_width=0),
-         psg.Button("Stop", disabled=True, button_color=("white", "black"),border_width=0)]
+         psg.Button("Stop", disabled=True, button_color=("white", "black"), border_width=0)]
     ]
 
     layout = [
-        [psg.Push(), psg.Image('CloseWindow.png',
-                             pad=0,
-                             enable_events=True,
-                             key="-CLOSE-")],
+        [psg.Push(), psg.Image("close.png", pad=0, enable_events=True, key="-CLOSE-")],
         [psg.TabGroup([[psg.Tab("Clock", clock_layout, element_justification="center"),
-                       psg.Tab("Alarm", alarm_layout)]])]]
+                       psg.Tab("Alarm", alarm_layout)]])]
+    ]
 
     return psg.Window(
-        "", layout, size=(520, 240), finalize=True, no_titlebar=True)
+        "Alarm Clock", layout, size=(520, 240),
+        finalize=True,
+        no_titlebar=True,
+        grab_anywhere=True,
+        element_justification='center')
 
 
-# Window setup
-window = create_window()
+def update_window(window, list_of_alarms):
+    # Update the main window with the current time and the list of alarms
 
-
-list_of_alarms = []
-
-
-def get_time():
-    # Updates the time, checks if alarm should go off
-
-    global list_of_alarms
     now = time.strftime("%H:%M:%S")
     window["-TIME-"].update(now)
 
-    if list_of_alarms:
-        for index, alarm in enumerate(list_of_alarms):
-            if now == alarm:
-                winsound.PlaySound("alarm.wav", winsound.SND_FILENAME)
-                window["Stop"].update(disabled=False)
-                list_of_alarms = list_of_alarms[index:]
-                update()
+    window["-ALARMS-LIST-"].update("")
+    for alarm in list_of_alarms:
+        window["-ALARMS-LIST-"].update(window["-ALARMS-LIST-"].get() + alarm + '\t')
 
 
-def confirm_alarm():
-    # Obtains the alarm time, adds it to the list
+def play_alarm():
+    # Play the alarm sound
 
-    hour_val = values["-HR-"]
-    minute_val = values["-MIN-"]
-
-    if hour_val and minute_val:
-        list_of_alarms.append(f"{hour_val}:{minute_val}:00")
-    update()
+    winsound.PlaySound("alarm.wav", winsound.SND_FILENAME)
 
 
-def stop_alarm():
-    # Stops the alarm currently going off
+def confirm_alarm(window, list_of_alarms, values):
+    # Add a new alarm to the list
+
+    hour_alarm = values["-HR-"]
+    minute_alarm = values["-MIN-"]
+
+    if hour_alarm and minute_alarm:
+        list_of_alarms.append(f"{hour_alarm}:{minute_alarm}:00")
+
+    update_window(window, list_of_alarms)
+
+
+def stop_alarm(window, list_of_alarms):
+    # Stop the currently playing alarm
 
     window["Stop"].update(disabled=True)
     list_of_alarms.pop(0)
-    update()
+    update_window(window, list_of_alarms)
 
 
-def update():
-    # Updates the list of alarms
+def main():
+    # Main function to run the alarm clock application
 
-    window["-LST-"].update("")
-    for alarm in list_of_alarms:
-        window["-LST-"].update(window["-LST-"].get() + alarm + '\t')
+    psg.theme("black")
+    window = create_window()
+    list_of_alarms = []
+
+    while True:
+        event, values = window.read(timeout=10)
+        if event in (psg.WIN_CLOSED, "-CLOSE-"):
+            break
+
+        update_window(window, list_of_alarms)
+
+        if list_of_alarms:
+            current_time = time.strftime("%H:%M:%S")
+            if current_time == list_of_alarms[0]:
+                play_alarm()
+                window["Stop"].update(disabled=False)
+
+        if event == "Confirm":
+            confirm_alarm(window, list_of_alarms, values)
+
+        if event == "Stop":
+            stop_alarm(window, list_of_alarms)
+
+    window.close()
 
 
-# Main loop
-while True:
-    event, values = window.read(timeout=10)
-    if event in (psg.WIN_CLOSED, "-CLOSE-"):
-        break
-
-    if event == "Confirm":
-        confirm_alarm()
-    if event == "Stop":
-        stop_alarm()
-
-    get_time()
-
-window.close()
+if __name__ == "__main__":
+    main()
